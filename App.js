1774998d29cf29145a4853bfd60865d248f62906
Avanaganti-Saichand <63704295+Avanaganti-Saichand@users.noreply.github.com>
2022-02-17 14:18:36 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+function SignIn() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>SignIn!</Text>
+    </View>
+  );
+}
+
+function SignUp() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>SignUp!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="SignIn" component={SignIn} />
+        <Tab.Screen name="SignUp" component={SignUp} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
